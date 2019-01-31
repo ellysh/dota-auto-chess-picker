08f23bfb62ad0ac72692b0b1595a6e09222ea74f
Ilya Shpigor <petrsum@gmail.com>
2019-01-31 21:27:06 +0100

    Add the button with the image

--- picker.py
+++ picker.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 
 from tkinter import *
+from tkinter import ttk
 from PIL import ImageTk,Image
 
 window = Tk()
 
 window.title("Welcome to LikeGeeks app")
 
-canvas = Canvas(window, width = 300, height = 300)
-canvas.pack()
+button = ttk.Button(window)
+button.pack()
 
 img = ImageTk.PhotoImage(Image.open("images/heroes.jpg"))
-canvas.create_image(20,20, anchor=NW, image=img)
+button.config(image = img)
 
 window.mainloop()
