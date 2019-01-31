c83e17e82345d230680f236d71c2593789bb1ca3
Ilya Shpigor <petrsum@gmail.com>
2019-01-31 22:03:06 +0100

    Implement the add_button function

--- picker.py
+++ picker.py
@@ -7,16 +7,21 @@ from PIL import ImageTk,Image
 def clicked():
   print("button was clicked")
 
+def add_button(window, handler, hero_name):
+  button = ttk.Button(window)
+  button.pack()
+
+  img = ImageTk.PhotoImage(Image.open("images/" + hero_name + ".png"))
+  button.config(image = img, command = handler)
+
+  return button, img
+
 def make_window():
   window = Tk()
 
   window.title("Welcome to LikeGeeks app")
 
-  button = ttk.Button(window)
-  button.pack()
-
-  img = ImageTk.PhotoImage(Image.open("images/abaddon.png"))
-  button.config(image = img, command = clicked)
+  abaddon_button, abaddon_img = add_button(window, clicked, "abaddon")
 
   window.mainloop()
 
