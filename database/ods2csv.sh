800d58610b66af142e8a5706e107429fcac80beb
Ilya Shpigor <petrsum@gmail.com>
2019-02-01 20:52:53 +0100

    Add scripts for generating CSV files from the Excel sheet

---
+++ database/ods2csv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+
+DATABASE_DIR="docs"
+TEMP_DIR="temp"
+CSV_DIR="csv"
+
+ssconvert -S -O 'separator=;' "$DATABASE_DIR/Database.ods" "$TEMP_DIR/Database.txt"
+
+cp "$TEMP_DIR/Database.txt.0" "$CSV_DIR/pieces.csv"
+cp "$TEMP_DIR/Database.txt.1" "$CSV_DIR/species.csv"
+cp "$TEMP_DIR/Database.txt.2" "$CSV_DIR/classes.csv"
