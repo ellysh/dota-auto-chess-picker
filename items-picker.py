b7185f914442cfc7de814baa5ada793b958940da
Ilya Shpigor <petrsum@gmail.com>
2019-02-03 21:51:31 +0100

    Fix widgets in the items-picker.py script

--- items-picker.py
+++ items-picker.py
@@ -5,23 +5,16 @@ from csv import reader
 from PIL import ImageTk,Image
 
 _VERSION = "0.2"
-_PIECES_FILE = "database/csv/pieces.csv"
-_SPECIES_FILE = "database/csv/species.csv"
-_CLASSES_FILE = "database/csv/classes.csv"
+_ITEMS_FILE = "database/csv/items.csv"
 _DEFAULT_COLOR = "#d9d9d9"
 
-PIECES = {}
-SPECIES = {}
-CLASSES = {}
+ITEMS = {}
 
-SPECIES_DESCRIPTION_1 = None
-SPECIES_NUMBER_1 = None
+ITEM_DESCRIPTION_1 = None
+ITEM_NUMBER_1 = None
 
-SPECIES_DESCRIPTION_2 = None
-SPECIES_NUMBER_2 = None
-
-CLASS_DESCRIPTION = None
-CLASS_NUMBER = None
+ITEM_DESCRIPTION_2 = None
+ITEM_NUMBER_2 = None
 
 BUTTONS = {}
 
@@ -31,22 +24,12 @@ def load_table(filename, table):
     next(csv_file)
 
     for line in csv_reader:
-      table[line[0]] = [line[1], line[2]]
+      table[line[1]] = [line[2], line[3]]
 
-def load_pieces():
-  global PIECES
+def load_items():
+  global ITEMS
 
-  load_table(_PIECES_FILE, PIECES)
-
-def load_species():
-  global SPECIES
-
-  load_table(_SPECIES_FILE, SPECIES)
-
-def load_classes():
-  global CLASSES
-
-  load_table(_CLASSES_FILE, CLASSES)
+  load_table(_ITEMS_FILE, ITEMS)
 
 def reset_all_buttons():
   global BUTTONS
@@ -54,44 +37,13 @@ def reset_all_buttons():
   for key, value in BUTTONS.iteritems():
     value[0].config(bg = _DEFAULT_COLOR)
 
-def highlight_similarity(piece_name, index, first_color, second_color):
-  global PIECES
-
-  similarity_list = PIECES[piece_name][index].split('/')
-
-  for key, value in BUTTONS.iteritems():
-    check_similarity_list = PIECES[key][index].split('/')
-
-    if key != piece_name:
-      color = _DEFAULT_COLOR
-
-      if similarity_list[0] in check_similarity_list:
-        color = first_color
-      elif 1 < len(similarity_list) \
-           and similarity_list[1] in check_similarity_list:
-        color = second_color
-
-      if value[0].cget("bg") == _DEFAULT_COLOR:
-        value[0].config(bg = color)
-      elif color != _DEFAULT_COLOR:
-        value[0].config(bg = "#7742f4")
-
-def highlight_species(piece_name):
-  highlight_similarity(piece_name, 0, "green", "yellow")
-
-  highlight_similarity(piece_name, 1, "blue", "black")
-
 def button_click(piece_name):
   global BUTTONS
-  global SPECIES_DESCRIPTION_1
-  global SPECIES_NUMBER_1
-  global SPECIES_DESCRIPTION_2
-  global SPECIES_NUMBER_2
-  global CLASS_DESCRIPTION
-  global CLASS_NUMBER
-  global PIECES
-  global SPECIES
-  global CLASSES
+  global ITEM_DESCRIPTION_1
+  global ITEM_NUMBER_1
+  global ITEM_DESCRIPTION_2
+  global ITEM_NUMBER_2
+  global ITEMS
 
   reset_all_buttons()
 
@@ -117,21 +69,19 @@ def add_button(window, handler, piece_name, column, row):
   button = Button(window)
   button.grid(column = column, row = row)
 
-  img = ImageTk.PhotoImage(Image.open("images/pieces/" + piece_name + ".png"))
+  img = ImageTk.PhotoImage(Image.open("images/items/" + piece_name + ".png"))
   button.config(image = img, command = lambda:handler(piece_name))
 
   return button, img
 
 def make_window():
   global VERSION
-  global PIECES
+  global ITEMS
   global BUTTONS
-  global SPECIES_DESCRIPTION_1
-  global SPECIES_NUMBER_1
-  global SPECIES_DESCRIPTION_2
-  global SPECIES_NUMBER_2
-  global CLASS_DESCRIPTION
-  global CLASS_NUMBER
+  global ITEM_DESCRIPTION_1
+  global ITEM_NUMBER_1
+  global ITEM_DESCRIPTION_2
+  global ITEM_NUMBER_2
 
   window = Tk()
 
@@ -140,8 +90,8 @@ def make_window():
   row = 0
   column = 0
 
-  for piece in PIECES:
-    BUTTONS[piece] = add_button(window, button_click, piece, row, column)
+  for item in ITEMS:
+    BUTTONS[item] = add_button(window, button_click, item, row, column)
 
     row += 1
 
@@ -190,11 +140,7 @@ matches", font=("Arial Bold", 12), wraplength=300, anchor=NW, \
   window.mainloop()
 
 def main():
-  load_pieces()
-
-  load_species()
-
-  load_classes()
+  load_items()
 
   make_window()
 
