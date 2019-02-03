dcd39d225ae597ac28c43601736d38de4cc9b53e
Ilya Shpigor <petrsum@gmail.com>
2019-02-03 15:42:32 +0100

    Generate the CSV file with the ITEMS table

--- database/ods2csv.sh
+++ database/ods2csv.sh
@@ -9,3 +9,4 @@ ssconvert -S -O 'separator=;' "$DATABASE_DIR/Database.ods" "$TEMP_DIR/Database.t
 cp "$TEMP_DIR/Database.txt.0" "$CSV_DIR/pieces.csv"
 cp "$TEMP_DIR/Database.txt.1" "$CSV_DIR/species.csv"
 cp "$TEMP_DIR/Database.txt.2" "$CSV_DIR/classes.csv"
+cp "$TEMP_DIR/Database.txt.3" "$CSV_DIR/items.csv"
