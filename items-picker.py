c87adf4ed4997adb7c1d37bada72b0bc44feea5d
Ilya Shpigor <petrsum@gmail.com>
2019-02-04 19:45:07 +0100

    Remove the deprecated code from the items-picker.py script

--- items-picker.py
+++ items-picker.py
@@ -9,13 +9,6 @@ _ITEMS_FILE = "database/csv/items.csv"
 _DEFAULT_COLOR = "#d9d9d9"
 
 ITEMS = {}
-
-ITEM_DESCRIPTION_1 = None
-ITEM_NUMBER_1 = None
-
-ITEM_DESCRIPTION_2 = None
-ITEM_NUMBER_2 = None
-
 BUTTONS = {}
 
 def load_table(filename, table):
@@ -37,40 +30,25 @@ def reset_all_buttons():
   for key, value in BUTTONS.iteritems():
     value[0].config(bg = _DEFAULT_COLOR)
 
-def button_click(piece_name):
+def highlight_items(item_name):
+  pass
+
+def button_click(item_name):
   global BUTTONS
-  global ITEM_DESCRIPTION_1
-  global ITEM_NUMBER_1
-  global ITEM_DESCRIPTION_2
-  global ITEM_NUMBER_2
   global ITEMS
 
   reset_all_buttons()
 
-  BUTTONS[piece_name][0].config(bg = "red")
+  BUTTONS[item_name][0].config(bg = "red")
 
-  highlight_species(piece_name)
+  highlight_items(item_name)
 
-  species = PIECES[piece_name][0].split('/')
-  SPECIES_NUMBER_1.config(text = SPECIES[species[0]][0])
-  SPECIES_DESCRIPTION_1.config(text = SPECIES[species[0]][1])
-
-  if 1 < len(species):
-    SPECIES_NUMBER_2.config(text = SPECIES[species[1]][0])
-    SPECIES_DESCRIPTION_2.config(text = SPECIES[species[1]][1])
-  else:
-    SPECIES_NUMBER_2.config(text = "")
-    SPECIES_DESCRIPTION_2.config(text = "")
-
-  CLASS_NUMBER.config(text = CLASSES[PIECES[piece_name][1]][0])
-  CLASS_DESCRIPTION.config(text = CLASSES[PIECES[piece_name][1]][1])
-
-def add_button(window, handler, piece_name, column, row):
+def add_button(window, handler, item_name, column, row):
   button = Button(window)
   button.grid(column = column, row = row)
 
-  img = ImageTk.PhotoImage(Image.open("images/items/" + piece_name + ".png"))
-  button.config(image = img, command = lambda:handler(piece_name))
+  img = ImageTk.PhotoImage(Image.open("images/items/" + item_name + ".png"))
+  button.config(image = img, command = lambda:handler(item_name))
 
   return button, img
 
@@ -78,10 +56,6 @@ def make_window():
   global VERSION
   global ITEMS
   global BUTTONS
-  global ITEM_DESCRIPTION_1
-  global ITEM_NUMBER_1
-  global ITEM_DESCRIPTION_2
-  global ITEM_NUMBER_2
 
   window = Tk()
 
@@ -98,44 +72,6 @@ def make_window():
     if 10 < row:
       row = 0
       column += 1
-
-  color1 = Label(window, bg = "green", width = 4, height = 1)
-  color1.grid(column = 0, row = 12)
-
-  SPECIES_NUMBER_1 = Label(window, font=("Arial Bold", 12))
-  SPECIES_NUMBER_1.grid(column = 1, row = 12, columnspan = 2)
-
-  SPECIES_DESCRIPTION_1 = Label(window, font=("Arial Bold", 12), \
-                                wraplength=300, anchor=NW, justify=LEFT)
-  SPECIES_DESCRIPTION_1.grid(column = 3, row = 12, columnspan = 10)
-
-  color2 = Label(window, bg = "yellow", width = 4, height = 1)
-  color2.grid(column = 0, row = 13)
-
-  SPECIES_NUMBER_2 = Label(window, font=("Arial Bold", 12))
-  SPECIES_NUMBER_2.grid(column = 1, row = 13, columnspan = 2)
-
-  SPECIES_DESCRIPTION_2 = Label(window, font=("Arial Bold", 12), \
-                                wraplength=300, anchor=NW, justify=LEFT)
-  SPECIES_DESCRIPTION_2.grid(column = 3, row = 13, columnspan = 10)
-
-  color3 = Label(window, bg = "blue", width = 4, height = 1)
-  color3.grid(column = 0, row = 14)
-
-  CLASS_NUMBER = Label(window, font=("Arial Bold", 12))
-  CLASS_NUMBER.grid(column = 1, row = 14, columnspan = 2)
-
-  CLASS_DESCRIPTION = Label(window, font=("Arial Bold", 12), \
-                            wraplength=300, anchor=NW, justify=LEFT)
-  CLASS_DESCRIPTION.grid(column = 3, row = 14, columnspan = 10)
-
-  color4 = Label(window, bg = "#7742f4", width = 4, height = 1)
-  color4.grid(column = 0, row = 15)
-
-  both_description = Label(window, text = "Both species and class \
-matches", font=("Arial Bold", 12), wraplength=300, anchor=NW, \
-    justify=LEFT)
-  both_description.grid(column = 1, row = 15, columnspan = 10)
 
   window.mainloop()
 
