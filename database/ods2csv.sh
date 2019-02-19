13d8776d7cc7425e9ff9e9f40abe6e9194b39bb2
Ilya Shpigor <petrsum@gmail.com>
2019-02-19 22:04:45 +0100

    Initial add the Combos table

--- database/ods2csv.sh
+++ database/ods2csv.sh
@@ -8,8 +8,11 @@ ssconvert -S -O 'separator=;' "$DATABASE_DIR/Database.ods" "$TEMP_DIR/Database.t
 
 ssconvert -S -O 'separator=;' "$DATABASE_DIR/Strategy.ods" "$TEMP_DIR/Strategy.txt"
 
+ssconvert -S -O 'separator=;' "$DATABASE_DIR/Combos.ods" "$TEMP_DIR/Combos.txt"
+
 cp "$TEMP_DIR/Database.txt.0" "$CSV_DIR/pieces.csv"
 cp "$TEMP_DIR/Database.txt.1" "$CSV_DIR/species.csv"
 cp "$TEMP_DIR/Database.txt.2" "$CSV_DIR/classes.csv"
 cp "$TEMP_DIR/Database.txt.3" "$CSV_DIR/items.csv"
 cp "$TEMP_DIR/Strategy.txt.0" "$CSV_DIR/strategies.csv"
+cp "$TEMP_DIR/Combos.txt.0" "$CSV_DIR/combos.csv"
