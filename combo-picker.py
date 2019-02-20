44250ee1df1e89a1fc1c6dd9d6abf50d0f5ff35b
Ilya Shpigor <petrsum@gmail.com>
2019-02-20 20:08:33 +0100

    Base implementation of the pieces-picker.py script

--- combo-picker.py
+++ combo-picker.py
@@ -11,7 +11,6 @@ _COMBOS_FILE = "database/csv/combos.csv"
 
 _DEFAULT_COLOR = "#d9d9d9"
 _AZURE_COLOR = "#5795f9"
-_BLUE_COLOR = "#144593"
 _GREEN_COLOR = "#66ce54"
 _PURPLE_COLOR = "#8757f9"
 _RED_COLOR = "#ff4f4f"
@@ -72,14 +71,31 @@ def highlight_species(piece_name):
        or (1 < len(species) and species[1] in PIECES[button[0]][0]):
       button[1].config(bg = _GREEN_COLOR)
 
+def highlight_class(piece_name):
+  global PIECES
+  global BUTTONS
+  global _AZURE_COLOR
+  global _PURPLE_COLOR
+
+  species = PIECES[piece_name][1].split('/')
+
+  for button in BUTTONS:
+    if species[0] in PIECES[button[0]][1] \
+       or (1 < len(species) and species[1] in PIECES[button[0]][1]):
+
+      if button[1].cget("bg") == _DEFAULT_COLOR:
+        color = _AZURE_COLOR
+      else:
+        color = _PURPLE_COLOR
+
+      button[1].config(bg = color)
+
 def button_click(piece_name):
   reset_all_buttons()
 
   highlight_species(piece_name)
 
-  #highlight_class(piece_name)
-
-  #highlight_species_and_class(piece_name)
+  highlight_class(piece_name)
 
   highlight_piece(piece_name)
 
