624b14ae50fd518ecd28bb543edc421358660577
Ilya Shpigor <petrsum@gmail.com>
2019-05-04 18:47:51 +0200

    Fix indents in the pieces_picker.py script

--- dac_picker/pieces_picker.py
+++ dac_picker/pieces_picker.py
@@ -31,96 +31,85 @@ SKILL_DESCRIPTION = None
 
 BUTTONS = {}
 
-def load_pieces():
-  global PIECES
-
-  load_table(_PIECES_FILE, PIECES, 4)
-
-def load_species():
-  global SPECIES
-
-  load_table(_SPECIES_FILE, SPECIES, 2)
-
-def load_classes():
-  global CLASSES
-
-  load_table(_CLASSES_FILE, CLASSES, 2)
-
 def reset_all_buttons():
-  global BUTTONS
+    global BUTTONS
 
-  for key, value in BUTTONS.items():
-    value[0].config(bg = _DEFAULT_COLOR)
+    for key, value in BUTTONS.items():
+        value[0].config(bg = _DEFAULT_COLOR)
+
 
 def highlight_similarity(piece_name, index, first_color, second_color):
-  global PIECES
-  global _PURPLE_COLOR
+    global PIECES
+    global _PURPLE_COLOR
 
-  similarity_list = PIECES[piece_name][index].split('/')
+    similarity_list = PIECES[piece_name][index].split('/')
 
-  for key, value in BUTTONS.items():
-    check_similarity_list = PIECES[key][index].split('/')
+    for key, value in BUTTONS.items():
+        check_similarity_list = PIECES[key][index].split('/')
 
-    if key != piece_name:
-      color = _DEFAULT_COLOR
+        if key != piece_name:
+            color = _DEFAULT_COLOR
 
-      if similarity_list[0] in check_similarity_list:
-        color = first_color
-      elif 1 < len(similarity_list) \
-           and similarity_list[1] in check_similarity_list:
-        color = second_color
+        if similarity_list[0] in check_similarity_list:
+            color = first_color
+        elif 1 < len(similarity_list) \
+             and similarity_list[1] in check_similarity_list:
+            color = second_color
 
-      if value[0].cget("bg") == _DEFAULT_COLOR:
-        value[0].config(bg = color)
-      elif color != _DEFAULT_COLOR:
-        value[0].config(bg = _PURPLE_COLOR)
+        if value[0].cget("bg") == _DEFAULT_COLOR:
+            value[0].config(bg = color)
+        elif color != _DEFAULT_COLOR:
+            value[0].config(bg = _PURPLE_COLOR)
+
 
 def highlight_species(piece_name):
-  global _AZURE_COLOR
-  global _BLUE_COLOR
-  global _GREEN_COLOR
-  global _YELLOW_COLOR
+    global _AZURE_COLOR
+    global _BLUE_COLOR
+    global _GREEN_COLOR
+    global _YELLOW_COLOR
 
-  highlight_similarity(piece_name, 0, _GREEN_COLOR, _YELLOW_COLOR)
+    highlight_similarity(piece_name, 0, _GREEN_COLOR, _YELLOW_COLOR)
 
-  highlight_similarity(piece_name, 1, _AZURE_COLOR, _BLUE_COLOR)
+    highlight_similarity(piece_name, 1, _AZURE_COLOR, _BLUE_COLOR)
+
 
 def button_click(piece_name):
-  global BUTTONS
-  global SPECIES_DESCRIPTION_1
-  global SPECIES_NUMBER_1
-  global SPECIES_DESCRIPTION_2
-  global SPECIES_NUMBER_2
-  global CLASS_DESCRIPTION
-  global CLASS_NUMBER
-  global SKILL_DESCRIPTION
-  global PIECES
-  global SPECIES
-  global CLASSES
-  global _RED_COLOR
+    global BUTTONS
+    global SPECIES_DESCRIPTION_1
+    global SPECIES_NUMBER_1
+    global SPECIES_DESCRIPTION_2
+    global SPECIES_NUMBER_2
+    global CLASS_DESCRIPTION
+    global CLASS_NUMBER
+    global SKILL_DESCRIPTION
+    global PIECES
+    global SPECIES
+    global CLASSES
+    global _RED_COLOR
 
-  reset_all_buttons()
+    reset_all_buttons()
 
-  BUTTONS[piece_name][0].config(bg = _RED_COLOR)
+    BUTTONS[piece_name][0].config(bg = _RED_COLOR)
 
-  highlight_species(piece_name)
+    highlight_species(piece_name)
 
-  species = PIECES[piece_name][0].split('/')
-  SPECIES_NUMBER_1.config(text = SPECIES[species[0]][0])
-  SPECIES_DESCRIPTION_1.config(text = SPECIES[species[0]][1])
+    species = PIECES[piece_name][0].split('/')
+    SPECIES_NUMBER_1.config(text = SPECIES[species[0]][0])
+    SPECIES_DESCRIPTION_1.config(text = SPECIES[species[0]][1])
 
-  if 1 < len(species):
-    SPECIES_NUMBER_2.config(text = SPECIES[species[1]][0])
-    SPECIES_DESCRIPTION_2.config(text = SPECIES[species[1]][1])
-  else:
-    SPECIES_NUMBER_2.config(text = "")
-    SPECIES_DESCRIPTION_2.config(text = "")
+    if 1 < len(species):
+        SPECIES_NUMBER_2.config(text = SPECIES[species[1]][0])
+        SPECIES_DESCRIPTION_2.config(text = SPECIES[species[1]][1])
+    else:
+        SPECIES_NUMBER_2.config(text = "")
+        SPECIES_DESCRIPTION_2.config(text = "")
 
-  if PIECES[piece_name][1] in CLASSES.keys():
-      CLASS_NUMBER.config(text = CLASSES[PIECES[piece_name][1]][0])
-      CLASS_DESCRIPTION.config(text = CLASSES[PIECES[piece_name][1]][1])
+    if PIECES[piece_name][1] in CLASSES.keys():
+        CLASS_NUMBER.config(text = CLASSES[PIECES[piece_name][1]][0])
+        CLASS_DESCRIPTION.config(text = CLASSES[PIECES[piece_name][1]][1])
 
-  SKILL_DESCRIPTION.config(text = PIECES[piece_name][3])
+    SKILL_DESCRIPTION.config(text = PIECES[piece_name][3])
+
 
 def add_button(window, button_click, piece, level, column, row):
     button = Button(window)
@@ -136,99 +125,105 @@ def add_button(window, button_click, piece, level, column, row):
 
     return button, img
 
+
 def add_buttons(window):
-  global BUTTONS
-  global PIECES
+    global BUTTONS
+    global PIECES
 
-  row = 0
-  column = 0
+    row = 0
+    column = 0
 
-  for key, value in PIECES.items():
-    BUTTONS[key] = add_button(window, button_click, key, value[2], \
-                              column, row)
+    for key, value in PIECES.items():
+        BUTTONS[key] = add_button(window, button_click, key, value[2], \
+                                column, row)
 
-    column += 1
+        column += 1
 
-    if 10 < column:
-      column = 0
-      row += 1
+        if 10 < column:
+            column = 0
+            row += 1
+
 
 def make_window():
-  global VERSION
-  global SPECIES_DESCRIPTION_1
-  global SPECIES_NUMBER_1
-  global SPECIES_DESCRIPTION_2
-  global SPECIES_NUMBER_2
-  global CLASS_DESCRIPTION
-  global CLASS_NUMBER
-  global SKILL_DESCRIPTION
-  global _PURPLE_COLOR
-  global _GREEN_COLOR
-  global _YELLOW_COLOR
-  global _AZURE_COLOR
-  global _RED_COLOR
+    global VERSION
+    global SPECIES_DESCRIPTION_1
+    global SPECIES_NUMBER_1
+    global SPECIES_DESCRIPTION_2
+    global SPECIES_NUMBER_2
+    global CLASS_DESCRIPTION
+    global CLASS_NUMBER
+    global SKILL_DESCRIPTION
+    global _PURPLE_COLOR
+    global _GREEN_COLOR
+    global _YELLOW_COLOR
+    global _AZURE_COLOR
+    global _RED_COLOR
 
-  window = Tk()
+    window = Tk()
 
-  window.title("Dota Auto Chess Pieces Picker " + VERSION)
+    window.title("Dota Auto Chess Pieces Picker " + VERSION)
 
-  buttons_frame = Frame(height = 2, bd = 1, relief = "sunken")
-  buttons_frame.pack(fill = "both", expand = True)
+    buttons_frame = Frame(height = 2, bd = 1, relief = "sunken")
+    buttons_frame.pack(fill = "both", expand = True)
 
-  add_buttons(buttons_frame)
+    add_buttons(buttons_frame)
 
-  info_frame = Frame(height = 2, bd = 1, relief = "sunken")
-  info_frame.pack(fill = "both", expand = True)
-
-
-  color1 = Label(info_frame, bg = _GREEN_COLOR, width = 4, height = 1)
-  color1.grid(column = 0, row = 0)
-
-  color2 = Label(info_frame, bg = _YELLOW_COLOR, width = 4, height = 1)
-  color2.grid(column = 0, row = 1)
-
-  color3 = Label(info_frame, bg = _AZURE_COLOR, width = 4, height = 1)
-  color3.grid(column = 0, row = 2)
-
-  color4 = Label(info_frame, bg = _PURPLE_COLOR, width = 4, height = 1)
-  color4.grid(column = 0, row = 3)
-
-  color5 = Label(info_frame, bg = _RED_COLOR, width = 4, height = 1)
-  color5.grid(column = 0, row = 4)
+    info_frame = Frame(height = 2, bd = 1, relief = "sunken")
+    info_frame.pack(fill = "both", expand = True)
 
 
-  SPECIES_NUMBER_1 = Label(info_frame, font=("Arial Bold", 12))
-  SPECIES_NUMBER_1.grid(column = 1, row = 0, sticky = 'W', padx = (10, 0))
+    color1 = Label(info_frame, bg = _GREEN_COLOR, width = 4, height = 1)
+    color1.grid(column = 0, row = 0)
 
-  SPECIES_NUMBER_2 = Label(info_frame, font=("Arial Bold", 12))
-  SPECIES_NUMBER_2.grid(column = 1, row = 1, sticky = 'W', padx = (10, 0))
+    color2 = Label(info_frame, bg = _YELLOW_COLOR, width = 4, height = 1)
+    color2.grid(column = 0, row = 1)
 
-  CLASS_NUMBER = Label(info_frame, font=("Arial Bold", 12))
-  CLASS_NUMBER.grid(column = 1, row = 2, sticky = 'W', padx = (10, 0))
+    color3 = Label(info_frame, bg = _AZURE_COLOR, width = 4, height = 1)
+    color3.grid(column = 0, row = 2)
+
+    color4 = Label(info_frame, bg = _PURPLE_COLOR, width = 4, height = 1)
+    color4.grid(column = 0, row = 3)
+
+    color5 = Label(info_frame, bg = _RED_COLOR, width = 4, height = 1)
+    color5.grid(column = 0, row = 4)
 
 
-  SPECIES_DESCRIPTION_1 = Label(info_frame, font=("Arial Bold", 12), \
-                                wraplength=300, anchor="nw", justify="left")
-  SPECIES_DESCRIPTION_1.grid(column = 2, row = 0, sticky = 'W', padx = (10, 0))
+    SPECIES_NUMBER_1 = Label(info_frame, font=("Arial Bold", 12))
+    SPECIES_NUMBER_1.grid(column = 1, row = 0, sticky = 'W', padx = (10, 0))
 
-  SPECIES_DESCRIPTION_2 = Label(info_frame, font=("Arial Bold", 12), \
-                                wraplength=300, anchor="nw", justify="left")
-  SPECIES_DESCRIPTION_2.grid(column = 2, row = 1, sticky = 'W', padx = (10, 0))
+    SPECIES_NUMBER_2 = Label(info_frame, font=("Arial Bold", 12))
+    SPECIES_NUMBER_2.grid(column = 1, row = 1, sticky = 'W', padx = (10, 0))
 
-  CLASS_DESCRIPTION = Label(info_frame, font=("Arial Bold", 12), \
-                            wraplength=300, anchor="nw", justify="left")
-  CLASS_DESCRIPTION.grid(column = 2, row = 2, sticky = 'W', padx = (10, 0))
+    CLASS_NUMBER = Label(info_frame, font=("Arial Bold", 12))
+    CLASS_NUMBER.grid(column = 1, row = 2, sticky = 'W', padx = (10, 0))
 
-  both_description = Label(info_frame, text = "Both species and class \
-matches", font=("Arial Bold", 12), wraplength=300, anchor="nw", \
-    justify="left")
-  both_description.grid(column = 2, row = 3, sticky = 'W', padx = (10, 0))
 
-  SKILL_DESCRIPTION = Label(info_frame, font=("Arial Bold", 12), \
-                            wraplength=300, anchor="nw", justify="left")
-  SKILL_DESCRIPTION.grid(column = 2, row = 4, sticky = 'W', padx = (10, 0))
+    SPECIES_DESCRIPTION_1 = Label(info_frame, font=("Arial Bold", 12),
+        wraplength=300, anchor="nw", justify="left")
 
-  window.mainloop()
+    SPECIES_DESCRIPTION_1.grid(column = 2, row = 0, sticky = 'W',
+        padx = (10, 0))
+
+    SPECIES_DESCRIPTION_2 = Label(info_frame, font=("Arial Bold", 12),
+        wraplength=300, anchor="nw", justify="left")
+
+    SPECIES_DESCRIPTION_2.grid(column = 2, row = 1, sticky = 'W', padx = (10, 0))
+
+    CLASS_DESCRIPTION = Label(info_frame, font=("Arial Bold", 12),
+        wraplength=300, anchor="nw", justify="left")
+
+    CLASS_DESCRIPTION.grid(column = 2, row = 2, sticky = 'W', padx = (10, 0))
+
+    both_description = Label(info_frame, text = "Both species and class matches",
+        font=("Arial Bold", 12), wraplength=300, anchor="nw",
+        justify="left")
+    both_description.grid(column = 2, row = 3, sticky = 'W', padx = (10, 0))
+
+    SKILL_DESCRIPTION = Label(info_frame, font=("Arial Bold", 12),
+        wraplength=300, anchor="nw", justify="left")
+    SKILL_DESCRIPTION.grid(column = 2, row = 4, sticky = 'W', padx = (10, 0))
+
+    window.mainloop()
 
 def main():
     global PIECES
